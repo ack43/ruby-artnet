1365bddf1e20844ef1a07b45211f5e6e43fd78e9
Tristan Harris <tris.dev@pumpcottage.me.uk>
2016-07-09 17:26:45 +0100

    Implement ArtAddress packet

--- lib/art_net/packet.rb
+++ lib/art_net/packet.rb
@@ -130,10 +130,32 @@ module ArtNet
 
     end
 
+    class Address < Base
+
+      attr_accessor :netswitch, :short_name, :long_name, :swin, :swout, :subswitch, :command
+
+      def initialize
+        @netswitch = 0
+        @short_name = ''
+        @long_name = ''
+        @swin = Array.new(4, 0x7f)
+        @swout = Array.new(4, 0x7f)
+        @subswitch = 0x7f
+        @command = 0
+      end
+
+      def pack
+        swvideo = 0
+        [ID, opcode, PROTVER, netswitch, short_name, long_name, *swin, *swout, subswitch, swvideo, command].pack "Z7xvnCxZ18Z64C4C4CCC"
+      end
+
+    end
+
     TYPES = {
       0x2000 => Poll,
       0x2100 => PollReply,
-      0x5000 => DMX
+      0x5000 => DMX,
+      0x6000 => Address
     }
 
     def self.load(data, sender)
